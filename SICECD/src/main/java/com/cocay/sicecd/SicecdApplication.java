a1d016d14029d8e9145abdafeb7320c6a7000788
JuanCarlos <JuanCarlos@Developer>
2019-03-26 14:01:26 -0600

    Add static resources to SicecdApplication class.

--- SICECD/src/main/java/com/cocay/sicecd/SicecdApplication.java
+++ SICECD/src/main/java/com/cocay/sicecd/SicecdApplication.java
@@ -4,12 +4,26 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @EnableWebMvc
 @ComponentScan
-public class SicecdApplication {
+public class SicecdApplication implements WebMvcConfigurer  {
     
+	@Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler(
+                "/css/**",
+                "/img/**",
+                "/js/**")
+                .addResourceLocations(
+                        "classpath:/static/css/",
+                        "classpath:/static/img/",
+                        "classpath:/static/js/");
+    }
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SicecdApplication.class, args);
 	}
