5b529afb676be00f81d687b5a5636e28359f14d3
JuanCarlos <JuanCarlos@Developer>
2019-03-28 16:25:44 -0600

    Update

--- SICECD/src/main/java/com/cocay/sicecd/SicecdApplication.java
+++ SICECD/src/main/java/com/cocay/sicecd/SicecdApplication.java
@@ -5,16 +5,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
 import org.thymeleaf.templatemode.TemplateMode;
 
-@SpringBootApplication
+@SpringBootApplication //same as @Configuration @EnableAutoConfiguration @ComponentScan
 @EnableWebMvc
-@ComponentScan
 public class SicecdApplication implements WebMvcConfigurer  {
     
 	@Autowired
