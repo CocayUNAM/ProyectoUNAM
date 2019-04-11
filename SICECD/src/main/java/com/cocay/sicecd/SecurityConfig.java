a87913b2275e3b99ca3294db8a8fd1e0a62d7e55
cderian <c.derian_11@ciencias.unam.mx>
2019-04-11 13:28:39 -0500

    Agregando consultas

--- SICECD/src/main/java/com/cocay/sicecd/SecurityConfig.java
+++ SICECD/src/main/java/com/cocay/sicecd/SecurityConfig.java
@@ -20,6 +20,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 	@Override
     protected void configure(HttpSecurity http) throws Exception {
         http
+        	.csrf().disable()
             .authorizeRequests()
                 .antMatchers("/", "/login", "/logout", "/css/**", "/fonts/**", "/img/**", "/js/**", "favicon.ico").permitAll()
                 .antMatchers("/table-export").hasAuthority("Administrador")
