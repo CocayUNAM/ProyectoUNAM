2a99bf6d4b28445bb98d99315ca858d15748450d
JCarlosHA <jc.hernandez.de.anda@gmail.com>
2019-04-11 17:49:03 -0500

    Update Examples

--- SICECD/src/main/java/com/cocay/sicecd/controller/WebController.java
+++ SICECD/src/main/java/com/cocay/sicecd/controller/WebController.java
@@ -16,13 +16,10 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.cocay.sicecd.model.Profesor;
 import com.cocay.sicecd.repo.ProfesorRep;
-import com.cocay.sicecd.repo.TestRepository;
 
 @Controller
 public class WebController {
 
-	@Autowired
-	TestRepository testRepository;
 	@Autowired
 	ProfesorRep test;
 	
@@ -53,28 +50,6 @@ public class WebController {
 	            cookie.setMaxAge(0);
 	        }
 		return "login";
-	}
-    
-	@RequestMapping(value = "/find-test", method = RequestMethod.GET)
-	public String findTest(Model model){
-		//http://localhost:8080/find-test
-        model.addAttribute("testCollection", testRepository.findAll());
-		return "example/test";
-	}
-	
-	@RequestMapping(value = "/table-row-select", method = RequestMethod.GET)
-	public String exampleTableRowSelect(Model model){
-		return "example/table-row-select";
-	}
-	
-	@RequestMapping(value = "/table-basic", method = RequestMethod.GET)
-	public String exampleTableBasic(Model model){
-		return "example/table-basic";
-	}
-	
-	@RequestMapping(value = "/table-export", method = RequestMethod.GET)
-	public String exampleExport(){
-		return "example/table-export";
 	}
 	
 	@RequestMapping(value = "/consultarProfesor", method = RequestMethod.GET)
@@ -129,23 +104,4 @@ public class WebController {
 		return "example/consultarCurso";
 	}
 	
-	@RequestMapping(value = "/table-jsgrid", method = RequestMethod.GET)
-	public String exampleJsgrid(Model model){
-		return "example/table-jsgrid";
-	}
-	
-	@RequestMapping(value = "/form-basic", method = RequestMethod.GET)
-	public String exampleFormBasic(Model model){
-		return "example/form-basic";
-	}
-	
-	@RequestMapping(value = "/form-validation", method = RequestMethod.GET)
-	public String exampleFormValidation(Model model){
-		return "example/form-validation";
-	}
-	
-	@RequestMapping(value = "/blank", method = RequestMethod.GET)
-	public String exampleBlank(Model model){
-		return "example/blank";
-	}
 }
