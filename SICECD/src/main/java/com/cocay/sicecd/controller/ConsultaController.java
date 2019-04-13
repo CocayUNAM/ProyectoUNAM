50cbc604c1e6d542a4e688f65fb51374c4396f64
JCarlosHA <jc.hernandez.de.anda@gmail.com>
2019-04-12 22:31:17 -0500

    Update Profesor, ConsultaController and ProfesorRep

--- SICECD/src/main/java/com/cocay/sicecd/controller/ConsultaController.java
+++ SICECD/src/main/java/com/cocay/sicecd/controller/ConsultaController.java
@@ -1,5 +1,7 @@
 package com.cocay.sicecd.controller;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -21,4 +23,26 @@ public class ConsultaController {
 		return "consultas/consultas";
 	}
 
+	@RequestMapping(value = "/consultasProfesores", method = RequestMethod.GET)
+	public String consultasProfesores(Model model){
+		
+		return "consultas/profesores";
+	}
+	
+	@RequestMapping(value = "/resultadosProfesores", method = {RequestMethod.GET, RequestMethod.POST})
+	public String resultadosProfesores(Model model, HttpServletRequest request){
+		String nombre = request.getParameter("nombre");
+		String apellido_paterno = request.getParameter("apellido_paterno");
+		String apellido_materno = request.getParameter("apellido_materno");
+		
+		if(nombre == null) {nombre="";} else {nombre=nombre.toUpperCase();}
+		if(apellido_paterno == null) {apellido_paterno="";} else {apellido_paterno=apellido_paterno.toUpperCase();}
+		if(apellido_materno == null) {apellido_materno="";} else {apellido_materno=apellido_materno.toUpperCase();}
+	
+		System.out.println(nombre +"-___-"+ apellido_paterno +"-___-"+ apellido_materno);
+		
+		model.addAttribute("profesores", profesor.findByCompleteNameList(nombre, apellido_paterno, apellido_materno));
+		return "consultas/resultados-profesores";
+	}
+	
 }
--- SICECD/src/main/java/com/cocay/sicecd/repo/ProfesorRep.java
+++ SICECD/src/main/java/com/cocay/sicecd/repo/ProfesorRep.java
@@ -4,6 +4,8 @@ package com.cocay.sicecd.repo;
 
 
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
@@ -23,6 +25,15 @@ Profesor findByRfc(@Param("rfc")String rfc);
 Profesor findByCompleteName(@Param("nombre") String nombre,
                              @Param("apellido_paterno") String apellido_paterno,
                              @Param("apellido_materno") String apellido_materno);
+
+
+@Query("SELECT p FROM Profesor p "
+		+ "WHERE upper(p.nombre) LIKE CONCAT('%',:nombre,'%') "
+		+ "AND upper(p.apellido_paterno) LIKE CONCAT('%',:apellido_paterno,'%') "
+		+ "AND upper(p.apellido_materno) LIKE CONCAT('%',:apellido_materno,'%')")
+List<Profesor> findByCompleteNameList(@Param("nombre") String nombre,
+                         @Param("apellido_paterno") String apellido_paterno,
+                         @Param("apellido_materno") String apellido_materno);
 }
 	
 	
