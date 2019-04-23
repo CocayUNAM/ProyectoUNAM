4a85512042a0ffaf0fba7f2e2e623a285eb51126
cderian <c.derian_11@ciencias.unam.mx>
2019-04-22 19:42:46 -0500

    Consultando cursos

--- SICECD/src/main/java/com/cocay/sicecd/controller/ConsultaProfesorController.java
+++ SICECD/src/main/java/com/cocay/sicecd/controller/ConsultaProfesorController.java
@@ -3,7 +3,6 @@ package com.cocay.sicecd.controller;
 import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
--- SICECD/src/main/java/com/cocay/sicecd/repo/ProfesorRep.java
+++ SICECD/src/main/java/com/cocay/sicecd/repo/ProfesorRep.java
@@ -13,23 +13,19 @@ import com.cocay.sicecd.model.Profesor;
 @Repository
 public interface ProfesorRep extends PagingAndSortingRepository<Profesor, Integer>{
 
-@Query("SELECT p FROM Profesor p  where p.rfc = :rfc ")
-Profesor findByRfc(@Param("rfc")String rfc);
+	@Query("SELECT p FROM Profesor p")
+	List<Profesor> findAll();
+	
+	@Query("SELECT p FROM Profesor p where p.rfc = :rfc ")
+	Profesor findByRfc(@Param("rfc")String rfc);
 
-@Query("SELECT p FROM Profesor p WHERE p.fk_id_estado = ?1")
-List<Profesor> findByStateList(Integer estado);
-
-@Query("SELECT p FROM Profesor p "
-		+ "WHERE upper(p.nombre) LIKE CONCAT('%',:nombre,'%') "
-		+ "AND upper(p.apellido_paterno) LIKE CONCAT('%',:apellido_paterno,'%') "
-		+ "AND upper(p.apellido_materno) LIKE CONCAT('%',:apellido_materno,'%')")
-List<Profesor> findByCompleteNameList(@Param("nombre") String nombre,
-                         @Param("apellido_paterno") String apellido_paterno,
-                         @Param("apellido_materno") String apellido_materno);
-
-@Query("SELECT p FROM Profesor p")
-List<Profesor> findAll();
-
+	@Query("SELECT p FROM Profesor p "
+			+ "WHERE upper(p.nombre) LIKE CONCAT('%',:nombre,'%') "
+			+ "AND upper(p.apellido_paterno) LIKE CONCAT('%',:apellido_paterno,'%') "
+			+ "AND upper(p.apellido_materno) LIKE CONCAT('%',:apellido_materno,'%')")
+	List<Profesor> findByCompleteNameList(@Param("nombre") String nombre,
+					@Param("apellido_paterno") String apellido_paterno,
+					@Param("apellido_materno") String apellido_materno);
 }
 	
 	
