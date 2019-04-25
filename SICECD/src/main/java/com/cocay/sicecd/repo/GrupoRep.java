e15a5f369845e16b28063b9079aa8dd5efc6c6e9
cderian <c.derian_11@ciencias.unam.mx>
2019-04-24 20:17:02 -0500

    Cambiando consulta de grupos | Agregando vista de Inscripciones

--- SICECD/src/main/java/com/cocay/sicecd/repo/GrupoRep.java
+++ SICECD/src/main/java/com/cocay/sicecd/repo/GrupoRep.java
@@ -7,17 +7,16 @@ import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import com.cocay.sicecd.model.Curso;
 import com.cocay.sicecd.model.Grupo;
 
 @Repository
 public interface GrupoRep extends PagingAndSortingRepository<Grupo, Integer>{
 	//Grupo findByClave(String name);
 	
-	@Query("SELECT g FROM Grupo g where g.fk_id_curso = :fk_id_curso ")
-	List<Grupo> findByID(@Param("fk_id_curso") int fk_id_curso);
+	@Query("SELECT g FROM Grupo g")
+	List<Grupo> findAll();
 	
 	@Query("SELECT g FROM Grupo g where g.clave = :clave ")
 	List<Grupo> findByClave(@Param("clave") String clave);
-
-
 }
