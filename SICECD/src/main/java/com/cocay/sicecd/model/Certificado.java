ea605a4b29d25cfd412b811e43d8397c25d53532
Moctezuma19 <ometeotlurss@ciencias.unam.mx>
2019-04-26 12:16:40 -0500

    Corrige error

--- SICECD/src/main/java/com/cocay/sicecd/model/Certificado.java
+++ SICECD/src/main/java/com/cocay/sicecd/model/Certificado.java
@@ -6,7 +6,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
-import javax.persistence.OneToOne;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 import org.hibernate.annotations.LazyCollection;
