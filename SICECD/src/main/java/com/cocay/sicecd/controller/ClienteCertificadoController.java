281ac074b0193bf67e392a17ba501f53494c2d87
Moctezuma19 <ometeotlurss@ciencias.unam.mx>
2019-04-29 14:46:30 -0500

    Modifica opciones de seg., para acceso a paginas sobre constancias

--- SICECD/src/main/java/com/cocay/sicecd/controller/ClienteCertificadoController.java
+++ SICECD/src/main/java/com/cocay/sicecd/controller/ClienteCertificadoController.java
@@ -4,6 +4,8 @@ import com.cocay.sicecd.model.Curso;
 import com.cocay.sicecd.model.Certificado;
 import com.cocay.sicecd.model.Profesor;
 import com.cocay.sicecd.repo.CertificadoRep;
+import com.cocay.sicecd.repo.CursoRep;
+import com.cocay.sicecd.repo.ProfesorRep;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -31,18 +33,22 @@ import org.json.*;
 @Controller
 public class ClienteCertificadoController {
 
-	private final String RUTA_LOCAL = "/home/jrivera/";
-	private final String URL_RS = "http/localhost/moodle35/mod/simplecertificate/test.php?";
+	private final String RUTA_LOCAL = "/your/path/";
+	private final String URL_RS = "http://localhost/moodle3.5/mod/simplecertificate/wscertificado.php?";
 	@Autowired
 	CertificadoRep bd_certificado;
+	@Autowired
+	ProfesorRep bd_profesor;
+	@Autowired
+	CursoRep bd_curso;
 	/**
 	* Metodo que obtiene un certificado dado un correo y nombre de curso, el cual es depositado en alguna carpeta
 	* y guardada su direccion en una base de datos.
 	* @param correo Correo de usuario.
 	* @param nombre_curso Nombre del curso en el cual esta inscrito el usuario.
-	* @return Devuelve true si el certificado se obtuvo y se ha guardado en Disco y en la BD.
+	* @return Devuelve la ruta en el sistema de archivos si el certificado se obtuvo y se ha guardado en Disco y en la BD.
 	*/
-	public boolean obtenCertificado(Profesor profesor, Curso curso) throws Exception{
+	public String obtenCertificado(Profesor profesor, Curso curso) throws Exception{
 		
 		String correo = profesor.getCorreo();
 		String nombre_curso = curso.getNombre();
@@ -50,7 +56,9 @@ public class ClienteCertificadoController {
 		cert.setFk_id_curso(curso);
 		cert.setFk_id_profesor(profesor);
 		correo = correo.replaceFirst("@", "%40");
+		System.out.println(correo);
 		nombre_curso = nombre_curso.replaceAll(" ", "%20");
+		System.out.println(nombre_curso);
 		String url = URL_RS + "email=" + correo + "&nc="
 				+ nombre_curso;
 		@SuppressWarnings("deprecation")
@@ -61,16 +69,19 @@ public class ClienteCertificadoController {
 		String jsonText = "";
 		String linea = null;
 		while ((linea = rd.readLine()) != null) {
-			System.out.println(linea);
+			//System.out.println(linea);
 			jsonText += linea;
 		}
 		JSONObject json = new JSONObject(jsonText);
-		if((String)json.get("mensaje") != "NULL"){
-			return false;
+		String mensaje = (String)json.get("mensaje");
+		if(!mensaje.equals("NULL")){
+			return null;
 		}
 		String string_pdf = (String) json.get("bytespdf");
 		byte[] bytearray = java.util.Base64.getDecoder().decode(string_pdf);
-		String path = RUTA_LOCAL + profesor.getPk_id_profesor() + "_" + (String)json.get("nombre_archivo") + ".pdf";
+		String aux = (String)json.get("nombre_archivo");
+		String na = String.valueOf(java.util.Base64.getDecoder().decode(aux));
+		String path = RUTA_LOCAL + profesor.getPk_id_profesor() + "_" + na + ".pdf";
 		File out = new File(path);
 		try (FileOutputStream os = new FileOutputStream(out)) {
 			os.write(bytearray);
@@ -80,6 +91,27 @@ public class ClienteCertificadoController {
 		}
 		cert.setRuta(path);
 		bd_certificado.save(cert);
-		return true;
+		return path;
+		//return true;
+	}
+
+	@RequestMapping(value = "/certificado", method = RequestMethod.GET)
+	public String muestra(Model model){
+		return "Certificado/certificado";
+	}
+
+	@RequestMapping(value = "/certificadoRes", method = RequestMethod.GET)
+	public String procesaCertificado(Model model, HttpServletRequest request) throws Exception{
+		String correo = request.getParameter("correo");
+		String nombre_c = request.getParameter("nc");
+		Profesor profr = bd_profesor.findByCorreo(correo);//obtener usuario de base de datos
+		Curso curso = bd_curso.findByNombre(nombre_c);//obtener curso de bd
+		String path = obtenCertificado(profr,curso);
+		if(path != null){
+			model.addAttribute("mensaje",path);
+		} else {
+			model.addAttribute("mensaje","No se encontro el certificado!");
+		}
+		return "Certificado/resultado";
 	}
 }
