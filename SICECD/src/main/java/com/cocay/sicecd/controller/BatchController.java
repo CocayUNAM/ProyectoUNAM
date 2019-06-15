3b3a76735e4652c326bf962819f644a1a904674f
Juan Carlos Garduño Vargas <juuanc_vargas@ciencias.unam.mx>
2019-06-14 21:22:22 -0500

    JCGV: Mejoras Vistas Batch y Avisos Correo

--- SICECD/src/main/java/com/cocay/sicecd/controller/BatchController.java
+++ SICECD/src/main/java/com/cocay/sicecd/controller/BatchController.java
@@ -1,5 +1,7 @@
 package com.cocay.sicecd.controller;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -21,13 +23,19 @@ import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteExcep
 import org.springframework.batch.core.repository.JobRestartException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.cocay.sicecd.dto.CursoDto;
 import com.cocay.sicecd.model.Curso;
 import com.cocay.sicecd.model.Grupo;
 import com.cocay.sicecd.model.Profesor;
@@ -43,11 +51,12 @@ public class BatchController {
 	JobLauncher jobLauncher;
 
 	@Autowired
-	Job job;
+	@Qualifier("jobCurso")
+	Job jobCurso;
 	
-//	@Autowired
-//	@Qualifier("JobGrupo")
-//	Job jobGrupo;
+	@Autowired
+	@Qualifier("jobGrupo")
+	Job jobGrupo;
 	
 	@Autowired
 	CursoRep curso;
@@ -58,43 +67,47 @@ public class BatchController {
 	@Autowired
 	ProfesorRep profesor;
 	
-//	@RequestMapping(value = "/runjob", method = RequestMethod.GET)
-//	public ModelAndView handle(ModelMap model, HttpServletRequest request) throws Exception {
-//		Logger logger = LoggerFactory.getLogger(this.getClass());
-//		try {
-//			JobParameters jobParameters = new JobParametersBuilder().addLong("time", System.currentTimeMillis())
-//					.toJobParameters();
-//			jobLauncher.run(job, jobParameters);
-//		} catch (Exception e) {
-//			logger.info(e.getMessage());
-//		}
-//		return new ModelAndView("/BatchTemplate/consultarProfesor");
-//	}
-	
 	@RequestMapping(value = "/runjob", method = RequestMethod.GET)
-	public ModelAndView load(ModelMap model, HttpServletRequest request) throws JobParametersInvalidException, JobExecutionAlreadyRunningException, JobRestartException, JobInstanceAlreadyCompleteException{
-		Map<String, JobParameter> maps = new HashMap<>();
-        maps.put("time", new JobParameter(System.currentTimeMillis()));
-        JobParameters parameters = new JobParameters(maps);
-        JobExecution jobExecution = jobLauncher.run(job, parameters);
-
-        System.out.println("JobExecution: " + jobExecution.getStatus());
-
-        System.out.println("Batch is Running...");
-        while (jobExecution.isRunning()) {
-            System.out.println("...");
-        }
-        return new ModelAndView("/BatchTemplate/consultarProfesor");
+	public ModelAndView handle(ModelMap model, HttpServletRequest request) throws Exception {
+		Logger logger = LoggerFactory.getLogger(this.getClass());
+		try {
+			JobParameters jobParameters = new JobParametersBuilder().addLong("time", System.currentTimeMillis())
+					.toJobParameters();
+			jobLauncher.run(jobCurso, jobParameters);
+		} catch (Exception e) {
+			logger.info(e.getMessage());
+		}
+		return new ModelAndView("/BatchTemplate/consultarProfesor");
 	}
-    
 	
-	/*
-	@RequestMapping(value = "/vistaBatch", method = RequestMethod.GET)
-	public ModelAndView consultaCustomer(ModelMap model, HttpServletRequest request) {
-		
-		return new ModelAndView("/BatchTemplate/CargaBatch");
-		
-	}*/
+	@RequestMapping(value = "/jobGrupo", method = RequestMethod.GET)
+	public ModelAndView handleGrupo(ModelMap model, HttpServletRequest request) throws Exception {
+		Logger logger = LoggerFactory.getLogger(this.getClass());
+		try {
+			JobParameters jobParameters = new JobParametersBuilder().addLong("time", System.currentTimeMillis())
+					.toJobParameters();
+			jobLauncher.run(jobGrupo, jobParameters);
+		} catch (Exception e) {
+			logger.info(e.getMessage());
+		}
+		return new ModelAndView("/BatchTemplate/consultarGrupo");
+	}
+	
+//	@RequestMapping(value="/import/file/uploadToDB", method=RequestMethod.POST)
+//    public String create(@RequestParam("file") MultipartFile multipartFile) throws IOException, JobExecutionAlreadyRunningException, JobRestartException, JobInstanceAlreadyCompleteException, JobParametersInvalidException{
+//
+//        //Save multipartFile file in a temporary physical folder
+//        String path = new ClassPathResource("/tmpuploads").getURL().getPath();//it's assumed you have a folder called tmpuploads in the resources folder
+//        File fileToImport = new File(path + multipartFile.getOriginalFilename());
+//        
+//        //Launch the Batch Job
+//        JobExecution jobExecution = jobLauncher.run(jobCurso, new JobParametersBuilder()
+//                .addString("fullPathFileName", fileToImport.getAbsolutePath())
+//                .toJobParameters());        
+//
+//        return "OK";
+//    }
+	
 	
 	@RequestMapping(value = "/consultarCursos", method = RequestMethod.GET)
 	public ModelAndView consultarCursos(ModelMap model, HttpServletRequest request) {
@@ -132,6 +145,32 @@ public class BatchController {
 			
 		} else {
 			return new ModelAndView("/BatchTemplate/consultarProfesor");
+		}
+	}
+	
+	@RequestMapping(value = "/pruebaBatch", method = RequestMethod.GET)
+	public ModelAndView pruebaBatch(ModelMap model, HttpServletRequest request) {
+		
+		List<Profesor> list_p = profesor.loadAllProfesor();
+		if(!list_p.isEmpty()) {
+			model.put("profesores", list_p);
+			return new ModelAndView("/BatchTemplate/prueba", model);
+			
+		} else {
+			return new ModelAndView("/BatchTemplate/prueba");
+		}
+	}
+	
+	@RequestMapping(value = "/AvisosCorreo", method = RequestMethod.GET)
+	public ModelAndView avisos(ModelMap model, HttpServletRequest request) {
+		
+		List<Profesor> list_p = profesor.loadAllProfesor();
+		if(!list_p.isEmpty()) {
+			model.put("profesores", list_p);
+			return new ModelAndView("/CorreosTemplate/AvisosCorreo", model);
+			
+		} else {
+			return new ModelAndView("/CorreosTemplate/AvisosCorreo");
 		}
 	}
 	
---
+++ SICECD/src/main/java/com/cocay/sicecd/repo/GrupoBatchRep.java
@@ -0,0 +1,9 @@
+package com.cocay.sicecd.repo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.cocay.sicecd.model.Grupo;
+
+public interface GrupoBatchRep extends JpaRepository<Grupo, Integer>{
+
+}
