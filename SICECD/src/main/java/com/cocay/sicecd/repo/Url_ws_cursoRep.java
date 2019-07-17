1f14fb3bff14f012324df6320de6f80ee0f0700a
Héctor Santaella Marín <hectorsama@MacBook-Air-de-Hector.local>
2019-07-16 21:58:20 -0500

    Se agregan clases y consultas para cursos e inscripciones

---
+++ SICECD/src/main/java/com/cocay/sicecd/repo/Url_ws_cursoRep.java
@@ -0,0 +1,13 @@
+package com.cocay.sicecd.repo;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+import com.cocay.sicecd.model.Url_ws_curso;
+
+public interface Url_ws_cursoRep extends PagingAndSortingRepository<Url_ws_curso, Integer> {
+	@Query("SELECT c FROM Url_ws_inscripcion c where c.activa = true")
+	List<Url_ws_curso> findVarios();
+}
